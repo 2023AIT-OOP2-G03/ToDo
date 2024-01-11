f7e3f2f0f3ae0b023431281dd78cb9822599319b
Hyouhyan <hyouhyan1201@gmail.com>
2024-01-11 15:02:43 +0900

    Update userManager.py

--- modules/userManager.py
+++ modules/userManager.py
@@ -1,3 +1,29 @@
+import json
+
 # 作成
 def create(id, pw):
-    pass
+    if check(id):
+        print('ユーザーが存在します')
+        return False
+    else:
+        # users.jsonを読み込み
+        users = json.load(open('users.json', 'r'))
+        # ユーザーを作成
+        users[id]["pass"] = pw
+        # users.jsonを書き込み
+        json.dump(users, open('users.json', 'w'))
+        return True
+
+# ユーザーの存在を確認
+def check(id):
+    # users.jsonを読み込み
+    users = json.load(open('users.json', 'r'))
+    # ユーザーの存在を確認
+    if id in users:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    # ユーザーの存在を確認
+    print(check('test'))
