ca683fee8bf625e46864ee5a317529b5151af783
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-01-11 22:51:29 +0900

    登録＆ログイン

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__, static_folder='web/static', template_folder='web/templates
 
 #登録ページ
 @app.route('/registration', methods=["GET"])
-def registration_show():
-    return render_template("create.html", message="waegrhtjyfu")
+def registration_show(message=None):
+    return render_template("create.html", message=message)
 #登録ページ
 @app.route('/registration', methods=["POST"])
 def registration():
@@ -16,7 +16,7 @@ def registration():
     if (result==True):
         return render_template("login.html", message="登録完了しました")
     else:
-        return render_template("create.html", message=result)
+        return registration_show(result)
 
 #ログインページ
 @app.route('/', methods=["GET"])
@@ -26,14 +26,12 @@ def index(message=None):
 def login_():
     username = request.form.get('user', None)
     password = request.form.get('pw', None)
-    # if (username==None and password==None):
-    #     print("username or password is None")
-        # return index()
+    if (username==None and password==None): return index()
+
     result = login.login(username, password)
-    if (result==True):
-        return index(message="ログイン完了")
-    else:
-        return index(message=result)
+
+    if (result==True): return render_template("todo.html")
+    else: return index(message=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
