2099ce3edd6cf80aeb7f22c17baf5df8e3e2df8a
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-01-11 15:06:49 +0900

    -

--- web/main.py
+++ web/main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request
-from modules import user
+#関数名は仮
+from modules import temp as regi
 
 app = Flask(__name__)
 
 
 #登録ページ
-@app.route('/registration')
-def index():
-    return render_template("addressbook.html")
+@app.route('/registration', methods=["GET"])
+def registration_show():
+    return render_template("test/test.html")
+#登録ページ
+@app.route('/registration', methods=["POST"])
+def registration():
+    username = request.form.get('user', None)
+    password = request.form.get('pw', None)
+    #関数名は仮
+    result = regi.registration(username, password)
+    if result: index()
+    else: return render_template("registration_failure.html")
 
 #ログインページ
 @app.route('/')
