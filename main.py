19e6790ed1e3c38a8cc20eebb496388b833b02dc
KITUNE <yuyuyuya.masaki1@gmail.com>
2024-01-19 16:36:41 +0900

    -

--- main.py
+++ main.py
@@ -87,5 +87,4 @@ def delete_user():
     return render_template("admin.html", message="ユーザーが存在しません")
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
